253b4f44c4fab91edffcd5c46176532be1f11b3b
Namazu <yoshiki.ohmori@gmail.com>
2019-05-03 06:52:50 +0000

    add test code

--- typing.js
+++ typing.js
@@ -11,6 +11,21 @@ var wordmap = createWordMap();
 
 words.forEach(w => splitWord(w).forEach(s=>console.log(wordmap[s])));
 
+var itr = wordmap.keys()
+for(var i=0;i<Object.keys(wordmap).length;i++){
+  var k = itr.next().value;
+  if(!k || k.length!=2) continue;
+  var lhs = wordmap[k[0]];
+  var rhs = wordmap[k[1]];
+  lhs.forEach(l =>{
+    rhs.forEach(r => {
+      wordmap[k].push(l+r);
+    });
+  });
+}
+
+console.log(wordmap);
+
 function onKeyDown(){
   console.log(event.key);
 }
